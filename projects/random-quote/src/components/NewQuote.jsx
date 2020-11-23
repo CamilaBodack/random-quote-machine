fd7526a883072d3d692dedd489665d8f2559b252
CamilaBodack <camila.bodack@gmail.com>
2020-11-23 14:06:05 -0300

    add state and update state with randomQuote()

--- projects/random-quote/src/components/NewQuote.jsx
+++ projects/random-quote/src/components/NewQuote.jsx
@@ -3,29 +3,26 @@ import ReactDOM from 'react-dom';
 import quotes from './QuotesDB';
 
 export default class Quotes extends Component {
+    constructor(props) {
+        super(props);
 
-    // QUOTES STATE
+        this.state = {
+            quote: quotes[0].quote,
+            author: quotes[0].author
+        }
 
-    state = {
-        quote: quotes[0].quote,
-        author: quotes[0].author
+        this.randomQuote = this.randomQuote.bind(this);
     }
 
-    let randomInt = Math.floor(Math.random() * quotes.length);
-
-    randomQuote(randomInt){
-        this.setState((state) => {
-            quote: quotes[randomInt].quote,
-            author: quotes[randomInt].author
-        });
-    }
-
+    randomQuote() {
+        let randomInt = Math.floor(Math.random() * quotes.length);
+        this.setState({quote: quotes[randomInt].quote, author: quotes[randomInt].author})};
 
     render() {
         return (<div>
             <p id="text">{this.state.quote}</p>
             <p id="author">{this.state.author}</p>
             <button onClick={this.randomQuote}>New Quote</button>
-        </div>)
+        </div>);
     }
 }
