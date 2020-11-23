2380ae73877a03583652b14245cc4e7cae114e9f
CamilaBodack <camila.bodack@gmail.com>
2020-11-23 17:55:10 -0300

    deploy to netlify

--- src/components/NewQuote.jsx
+++ src/components/NewQuote.jsx
@@ -26,8 +26,7 @@ export default class Quotes extends Component {
             <p id="text">{this.state.quote}</p>
             <p id="author">{this.state.author}</p>
             <button id="new-quote" onClick={this.randomQuote}>New Quote</button>
-            <a id="tweet-quote" href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" target="_blank" data-show-count="false"><img src={twitter} alt="Twitter icon" width="30" height="30"/>Twitter</a>
-            <script async="async" src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank"><img src={twitter} alt="Twitter icon" width="30" height="30" />Twitter</a>
         </div>);
     }
 }
