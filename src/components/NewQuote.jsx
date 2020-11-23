92c449e77bfe231ad1dfce808f4b533adf30bde6
CamilaBodack <camila.bodack@gmail.com>
2020-11-23 17:22:23 -0300

    update twitter link

--- src/components/NewQuote.jsx
+++ src/components/NewQuote.jsx
@@ -18,14 +18,16 @@ export default class Quotes extends Component {
 
     randomQuote() {
         let randomInt = Math.floor(Math.random() * quotes.length);
-        this.setState({quote: quotes[randomInt].quote, author: quotes[randomInt].author})};
+        this.setState({quote: quotes[randomInt].quote, author: quotes[randomInt].author})
+    };
 
     render() {
         return (<div>
             <p id="text">{this.state.quote}</p>
             <p id="author">{this.state.author}</p>
             <button id="new-quote" onClick={this.randomQuote}>New Quote</button>
-            <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank"><img src={twitter} alt="Twitter icon" width="30" height="30" />Twitter</a>
+            <a id="tweet-quote" href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" target="_blank" data-show-count="false"><img src={twitter} alt="Twitter icon" width="30" height="30"/>Twitter</a>
+            <script async="async" src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </div>);
     }
 }
